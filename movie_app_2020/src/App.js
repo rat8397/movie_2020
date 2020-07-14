4b5f627a97de3091e61c7b204995ee0cc7c6e43a
rat8397 <wnsgur8397@daum.net>
2020-07-14 22:47:45 +0900

    life cycle

--- movie_app_2020/src/App.js
+++ movie_app_2020/src/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import Potato from './Potato'
 import PropTypes from "prop-types";
 class App extends React.Component{
+  constructor(props){
+    super(props)
+    console.log("CONSTRUCTOR");
+  }
   state = {
     count : 0
   }
@@ -13,7 +17,15 @@ class App extends React.Component{
     this.setState(current=>({count:current.count-1}));
 
   }
+  componentDidMount(){
+    console.log("component DID MOUNT")
+  }
+  componentDidUpdate(){
+    console.log("component DID UPDATE")
+  }
   render(){
+    console.log("render");
+
     return (
       <>
         <h1> JUNZZI APP </h1>
